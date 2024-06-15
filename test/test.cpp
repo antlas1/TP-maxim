62f1f185f29b34d3a788800e7179f33b22a4cc85
antlas <anton_lastochkin@mail.ru>
2024-06-15 08:26:42 +0300

    Исправление

--- test/test.cpp
+++ test/test.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleOutputCP(CP_UTF8);
+    std::cout << "Здравствуй мир!\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
